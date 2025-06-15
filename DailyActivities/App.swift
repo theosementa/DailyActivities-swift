ad2ec39b7589da590dd0e063a93bdbf64af64a71
Theo Sementa <theosementa@gmail.com>
2025-06-15 20:51:26 +0200

    first commit

---
+++ DailyActivities/App.swift
@@ -0,0 +1,19 @@
+//
+//  DailyActivitiesApp.swift
+//  DailyActivities
+//
+//  Created by Theo Sementa on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct DailyActivitiesApp: App {
+    
+    // MARK: - View
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

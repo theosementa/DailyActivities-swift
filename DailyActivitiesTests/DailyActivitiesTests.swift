fc3cf9856933dac3fadc79cb590609a3c1e81d42
Theo Sementa <theosementa@gmail.com>
2025-06-15 20:46:46 +0200

    Initial Commit

---
+++ DailyActivitiesTests/DailyActivitiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DailyActivitiesTests.swift
+//  DailyActivitiesTests
+//
+//  Created by Theo Sementa on 15/06/2025.
+//
+
+import Testing
+@testable import DailyActivities
+
+struct DailyActivitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
